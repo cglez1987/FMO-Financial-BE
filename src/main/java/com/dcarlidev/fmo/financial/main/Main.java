b9ba889cb8c690ca1e83df74802131028100242b
cglez1987 <cglez1987@gmailcom>
2019-05-05 15:55:35 -0400

    Created the ETL component executor

--- src/main/java/com/dcarlidev/fmo/financial/main/Main.java
+++ src/main/java/com/dcarlidev/fmo/financial/main/Main.java
@@ -7,88 +7,58 @@ package com.dcarlidev.fmo.financial.main;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.pentaho.di.core.KettleEnvironment;
-import org.pentaho.di.job.Job;
-import org.pentaho.di.job.JobMeta;
 
 /**
  *
- * @author CARLOGON
+ * @author carlos
  */
 public class Main {
 
     public static void main(String[] args) {
         try {
-            KettleEnvironment.init();
             String path = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
             File parent = new File(path).getParentFile();
-            File[] listFiles = parent.listFiles();
             String separator = System.getProperty("file.separator");
-            String jobName = "c";
-            for (File file : listFiles) {
-                if (file.getName().endsWith(".kjb")) {
-                    jobName = file.getName();
-                    break;
-                }
-                if (file.getName().endsWith(".ktr")) {
-                    jobName = file.getName();
-                    break;
-                }
-            }
-            if ("".equals(jobName)) {
-                throw new Exception("Transformation not found!");
-            } else {
-//                String job_file = parent.getPath() + separator + jobName;
-//                Properties properties = new Properties();
-//                InputStream fileProperties = new FileInputStream(parent.getPath() + separator + "config" + separator + "config.properties");
-//                properties.load(fileProperties);
-//                JobMeta jobMeta = new JobMeta(job_file, null);
-//                Job job = new Job(null, jobMeta);
-//                for (String prop : properties.stringPropertyNames()) {
-//                    if (prop.startsWith("parameter")) {
-//                        if (prop.contains("encrypt")) {
-//                            String parameter = prop.replaceFirst("parameter.encrypt.", "");
-//                            //String value = SecurityUtil.decrypt(properties.getProperty(prop));
-//                            //job.setParameterValue(parameter, value);
-//                        } else {
-//                            String parameter = prop.replaceFirst("parameter.", "");
-//                            job.setParameterValue(parameter, properties.getProperty(prop));
-//                        }
-//                    } else if (prop.startsWith("variable")) {
-//                        if (prop.contains("encrypt")) {
-//                            String variable = prop.replaceFirst("variable.encrypt.", "");
-//                            //String value = SecurityUtil.decrypt(properties.getProperty(prop));
-//                            //job.setVariable(variable, value);
-//                        } else {
-//                            String variable = prop.replaceFirst("variable.", "");
-//                            job.setVariable(variable, properties.getProperty(prop));
-//                        }
-//                    }
-//                }
-                JobMeta jobMeta = new JobMeta("C:\\Users\\lisbet\\Documents\\ETL\\FMO_Job.ktr.kjb", null);
-                Job job = new Job(null, jobMeta);
-                if (args.length > 0) {
-                    for (String var : args) {
-                        String varName = var.split("=")[0];
-                        String varValue = var.split("=")[1];
-                        job.setVariable(varName, varValue);
+
+            Properties properties = new Properties();
+            InputStream fileProperties = new FileInputStream(parent.getPath() + separator + "config" + separator + "config.properties");
+            properties.load(fileProperties);
+
+            HashMap<String, String> job_parameters = new HashMap<>();
+            HashMap<String, String> job_variables = new HashMap<>();
+
+            properties.stringPropertyNames().forEach(prop -> {
+                if (prop.startsWith("parameter")) {
+                    String parameter;
+                    String value;
+                    if (prop.contains("encrypt")) {
+                        parameter = prop.replaceFirst("parameter.encrypt.", "");
+                        //value = SecurityUtil.decrypt(properties.getProperty(prop));
+                        value = properties.getProperty(prop);
+                    } else {
+                        parameter = prop.replaceFirst("parameter.", "");
+                        value = properties.getProperty(prop);
                     }
+                    job_parameters.put(parameter, value);
                 }
-//                DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-//                Date date = new Date();
-//                String fecha = dateFormat.format(date);
-//                job.setVariable("fecha", fecha);
-                job.start();
-                job.waitUntilFinished();
-                if (job.isFinished() && job.getErrors() == 0) {
-                    System.out.println("Transformation is finished correctly!!!!");
-                } else {
-                    System.out.println("Error in transformation: \n");
+                if (prop.startsWith("variable")) {
+                    String variable;
+                    String value;
+                    if (prop.contains("encrypt")) {
+                        variable = prop.replaceFirst("variable.encrypt.", "");
+                        //value = SecurityUtil.decrypt(properties.getProperty(prop));
+                        value = properties.getProperty(prop);
+                    } else {
+                        variable = prop.replaceFirst("variable.", "");
+                        value = properties.getProperty(prop);
+                    }
+                    job_variables.put(variable, value);
                 }
-            }
+            });
         } catch (Exception ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
