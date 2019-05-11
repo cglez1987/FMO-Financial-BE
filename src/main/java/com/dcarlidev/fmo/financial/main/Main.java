aa0a20f5336149fb5a04d9d534d4662c6cc74616
cglez1987 <cglez1987@gmailcom>
2019-05-10 21:38:19 -0400

    Creating the call to ETL executor

--- src/main/java/com/dcarlidev/fmo/financial/main/Main.java
+++ src/main/java/com/dcarlidev/fmo/financial/main/Main.java
@@ -8,67 +8,98 @@ import com.dcarlidev.fmo.financial.etl.ETL_Executor;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  *
  * @author carlos
  */
-public class Main {
-    
-    public static void main(String[] args) {
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+
+    private static String name;
+
+    public Main(@Value("${spring.application.name}") String name) {
+        Main.name = name;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Main.execute();
+    }
+
+    public static void execute() {
         try {
             String main_path = Main.class.getProtectionDomain().getCodeSource().getLocation().getPath();
             File parent = new File(main_path).getParentFile();
             String separator = System.getProperty("file.separator");
-            
-            Properties properties = new Properties();
-            InputStream fileProperties = new FileInputStream(parent.getPath() + separator + "config" + separator + "config.properties");
-            properties.load(fileProperties);
-            
+
             HashMap<String, String> job_parameters = new HashMap<>();
             HashMap<String, String> job_variables = new HashMap<>();
-            
-            properties.stringPropertyNames().forEach(prop -> {
-                if (prop.startsWith("parameter")) {
-                    String parameter;
-                    String value;
-                    if (prop.contains("encrypt")) {
-                        parameter = prop.replaceFirst("parameter.encrypt.", "");
-                        //value = SecurityUtil.decrypt(properties.getProperty(prop));
-                        value = properties.getProperty(prop);
-                    } else {
-                        parameter = prop.replaceFirst("parameter.", "");
-                        value = properties.getProperty(prop);
+
+            System.out.println("File----" + name);
+            Properties properties = new Properties();
+            InputStream fileProperties = new FileInputStream(parent.getPath() + separator + "config" + separator + "config.properties");
+            System.out.println("FilePropeties: " + fileProperties.toString());
+            try {
+                properties.load(fileProperties);
+                properties.stringPropertyNames().forEach(prop -> {
+                    if (prop.startsWith("parameter")) {
+                        String parameter;
+                        String value;
+                        if (prop.contains("encrypt")) {
+                            parameter = prop.replaceFirst("parameter.encrypt.", "");
+                            //value = SecurityUtil.decrypt(properties.getProperty(prop));
+                            value = properties.getProperty(prop);
+                        } else {
+                            parameter = prop.replaceFirst("parameter.", "");
+                            value = properties.getProperty(prop);
+                        }
+                        job_parameters.put(parameter, value);
                     }
-                    job_parameters.put(parameter, value);
-                }
-                if (prop.startsWith("variable")) {
-                    String variable;
-                    String value;
-                    if (prop.contains("encrypt")) {
-                        variable = prop.replaceFirst("variable.encrypt.", "");
-                        //value = SecurityUtil.decrypt(properties.getProperty(prop));
-                        value = properties.getProperty(prop);
-                    } else {
-                        variable = prop.replaceFirst("variable.", "");
-                        value = properties.getProperty(prop);
+                    if (prop.startsWith("variable")) {
+                        String variable;
+                        String value;
+                        if (prop.contains("encrypt")) {
+                            variable = prop.replaceFirst("variable.encrypt.", "");
+                            //value = SecurityUtil.decrypt(properties.getProperty(prop));
+                            value = properties.getProperty(prop);
+                        } else {
+                            variable = prop.replaceFirst("variable.", "");
+                            value = properties.getProperty(prop);
+                        }
+                        job_variables.put(variable, value);
                     }
-                    job_variables.put(variable, value);
-                }
-            });
-            
-            String job_path = properties.getProperty("job_path");
-            
+                });
+            } catch (Exception ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            Date date = new Date();
+            String fecha = dateFormat.format(date);
+            job_parameters.put("actual_date", fecha);
+
+            String job_path = "";
+
             ETL_Executor etl_executor = new ETL_Executor();
             etl_executor.executeKettleJob(job_path, job_parameters, job_variables);
-            
         } catch (Exception ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+    }
+
+    public static void main(String... args) {
+        SpringApplication.run(Main.class, args);
     }
 }
